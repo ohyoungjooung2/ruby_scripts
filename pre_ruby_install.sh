50548e016c624044a88bbefaf8960cc945c4efff
ohyoungjooung2 <wnapdlf05@gmail.com>
2017-01-21 20:27:58 +0900

    new installing ruby script and adding pre-requsites depends on linux distro-centos(redhat)-ubuntu(debian)

--- pre_ruby_install.sh
+++ pre_ruby_install.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-if [[ /etc/redhat-release ]]
+if [[ -e /etc/redhat-release ]]
 then
+  sudo yum update
   sudo yum -y install wget gcc openssl-devel libffi-devel
+  echo "installing necessary packages"
+  sleep 3
+elif [[ `cat /proc/version | egrep -i "ubuntu|debian"` != "" ]]
+then
+  sudo apt-get -y update
+  sudo apt-get -y install libc6-dev libssl-dev libmysql++-dev libsqlite3-dev make build-essential libssl-dev libreadline6-dev zlib1g-dev libyaml-dev
+  echo "installing necessary packages"
+  sleep 3
 fi
